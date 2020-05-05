0178b0396588598292116bd3e115bd1a95a2abb4
Max Dayton <daytonmaximus@gmail.com>
2020-05-05 13:33:45 -0600

    navlinks added, active styling added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import classes from './NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => (
     <div className={classes.NavBar}>
         <div className={classes.NavItems}>
-            <a className={classes.Link} href='#home'>Home</a>
-            <a className={classes.Link} href='#about'>About</a>
+            <NavLink className={classes.Link} activeClassName={classes.ActiveLink} to='/' exact>Home <hr className={classes.ActiveUnderline} /></NavLink>
+            <NavLink className={classes.Link} activeClassName={classes.ActiveLink} to='/about'>About <hr className={classes.ActiveUnderline} /></NavLink>
             <span className={classes.CenterLogo}>
                 <p className={classes.Est}>EST. 2020</p>
                 <h3 className={classes.LogoHeader}>The Schwab Bakery</h3>
                 <p className={classes.Smithfield}><hr className={classes.SmithfieldHr} /> Smithfield UT <hr className={classes.SmithfieldHr} /></p>
             </span>
-            <a className={classes.Link} href='#menu'>Menu</a>
-            <a className={classes.Link} href='#contact'>Contact</a>
+            <NavLink className={classes.Link} activeClassName={classes.ActiveLink} to='/menu'>Menu <hr className={classes.ActiveUnderline} /></NavLink>
+            <NavLink className={classes.Link} activeClassName={classes.ActiveLink} to='/contact'>Contact <hr className={classes.ActiveUnderline} /></NavLink>
         </div>
     </div>
 );
