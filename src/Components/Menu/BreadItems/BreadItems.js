8014d29124928a9909317a3a0dd13b97f5f4d3e0
Max Dayton <daytonmaximus@gmail.com>
2020-05-17 18:29:05 -0600

    artisan note added

--- src/Components/Menu/BreadItems/BreadItems.js
+++ src/Components/Menu/BreadItems/BreadItems.js
@@ -6,7 +6,7 @@ const BreadItems = (props) => (
         <MenuItem name="100% Whole Wheat" desc="A loaf of pure wheat bread." price="$3.50" />
         <MenuItem name="50% Wheat/White" desc="Half wheat bread, half white bread." price="$3.50" />
         <MenuItem name="White Bread" desc="One pure white bread loaf." price="$3.00" />
-        <MenuItem name="Artisan Bread" desc="Our signature hand-crafted bread." price="$3.00" />
+        <MenuItem name="Artisan Bread" desc="Our signature hand-crafted bread. (Please order at least 1 day in advance.)" price="$3.00" />
     </div>
 );
 
